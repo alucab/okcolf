eca231bff50bf55c18d85612cacb58d33be4726e
Luca Barba <alucab@alucab.org>
2025-10-06 23:44:15 +0200

    added management of online/offline

--- js/conf.js
+++ js/conf.js
@@ -6,46 +6,57 @@
  ***********************************************************************/
 
 
+const appState = {
+  activePage: 'dashboard',
+  isEmployerConfigured: false,
+  isWorkerConfigured: false,
+  isContractConfigured: false,
+  isUserAnonymous: true,
+  isSocialSecurityDeadlinePassed: false,
+  isApplicationConnected: navigator.onLine,
+  workers: [],
 
+  setConnection(status) { 
+    this.isApplicationConnected = status 
+  },
+
+  setPage(page) { this.activePage = page.id || 'dashboard' },
+
+  async init() {
+    await window.db.open();
+    await window.KV.get("")
+    const workers = await window.db.workers.toArray();
+    if (workers.length) this.activeWorker = workers[0];
+    this.workers = workers;
+  }
+}
 
 
 $d.on('alpine:init', () => {
-  Alpine.store('appState', {
-    activePage: 'dashboard',
-    isEmployerConfigured: false,
-    isWorkerConfigured: false,
-    isContractConfigured: false,
-    isUserAnonymous: true,
-    isSocialSecurityDeadlinePassed: false,
-    isApplicationConnected: navigator.onLine,
-    workers: [],
 
-    setConnection(status) { this.isApplicationConnected = status },
-
-    setPage(page) { this.activePage = page.id || 'dashboard' },
-
-    async init() {
-      await window.db.open();
-      await window.KV.get("")
-      const workers = await window.AppDB.workers.toArray();
-      if (workers.length) this.activeWorker = workers[0];
-      this.workers = workers;
-    }
-  })
+  console.log('Alpine:init fired but ignored');
 });
 
 $d.on('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
   ons.ready(() => {
+    console.log('Onsen UI is ready');
     initApp();
-
+    if (window.Alpine && !Alpine.store('appState')) {
+      Alpine.store('appState', appState)
+    }
     // Listener globali dopo che Alpine e Onsen sono pronti
-    $w.on('online', () => Alpine.store('app').setConnection(true));
-    $w.on('offline', () => Alpine.store('app').setConnection(false));
+    $w.on('online', () => Alpine.store('appState').setConnection(true));
+    $w.on('offline', () => Alpine.store('appState').setConnection(false));
   });
 });
 
-$d.on('init', function(event) {
-  
+/*['DOMContentLoaded', 'alpine:init', 'init', 'onsready'].forEach(ev => {
+  window.addEventListener(ev, () => console.log(`🔥 ${ev}`));
+});*/
+
+$d.on('init', function (event) {
+
   Alpine.store('appState')?.setPage(event.target);
 
 });
